23791fa25bf2da52a12ab72caeeaa46815ffa1f7
Ivan Tan <ivanxtan@gmail.com>
2018-12-15 00:38:46 -0800

    Added method comments & added spacing

--- Participant.java
+++ Participant.java
@@ -20,42 +20,72 @@ public class Participant {
    // Participant object (Participant)
    private Participant sender;
    
+   // Constructor method
+   //    String name = the desired name for the Participant object
    public Participant(String name) {
       this.name = name;
    }
    
+   // Accessor/Getter method
+   //    Get the name of the Participant object
+   // Returns:
+   //    name of the Participant object (String)
    public String getName() {
       return name;
    }
    
+   // Checks if the Participant object has the recipient set
+   // Returns:
+   //    whether or not the recipient is set (boolean)
    public boolean hasRecipient() {
       return recipient != null;
    }
    
+   // Accessor/Getter method
+   //    Get the name of the recipient of the Participant object
+   // Returns:
+   //    name of the recipient of the Participant object (String)
    public String getRecipientName() {
       return recipient.getName();
    }
    
+   // Mutator/Setter method
+   //    Sets the recipient of the Participant object
+   // Parameters:
+   //    Participant recipient = the desired recipient
    public void setRecipient(Participant recipient) {
       this.recipient = recipient;
    }
    
+   // Resets the recipient of the Participant object
    public void resetRecipient() {
       this.recipient = null;
    }
    
+   // Checks if the Participant object has the sender set
+   // Returns:
+   //    whether or not the sender is set (boolean)
    public boolean hasSender() {
       return sender != null;
    }
    
+   // Accessor/Getter method
+   //    Get the name of the sender of the Participant object
+   // Returns:
+   //    name of the sender of the Participant object (String)
    public String getSenderName() {
       return sender.getName();
    }
    
+   // Mutator/Setter method
+   //    Sets the sender of the Participant object
+   // Parameters:
+   //    Participant sender = the desired sender
    public void setSender(Participant sender) {
       this.sender = sender;
    }
    
+   // Resets the sender of the Participant object
    public void resetSender() {
       this.sender = null;
    }
--- SecretSanta.java
+++ SecretSanta.java
@@ -16,8 +16,8 @@ import java.util.*;
 
 public class SecretSanta {
    // Represents the threshold multiplier to reset the loop when 
-   // an impossible matching scenario occurs.
-   public static final int RETRY_COUNT_MULTIPLIER = 1;
+   // an impossible matching scenario occurs
+   public static final int RETRY_COUNT_MULTIPLIER = 2;
 
    public static void main(String[] args) {
       Random rand = new Random();
@@ -26,11 +26,17 @@ public class SecretSanta {
       intro();
       
       Participant[] participants = createParticipants(console);
-
+      
+      /* 
+      
+      // Create n filler participants with format "Participant #x"
+      
       participants = new Participant[1000];
       for (int i = 0; i < participants.length; i++) {
          participants[i] = new Participant("Participant #" + (i + 1));
       }
+      
+      */
       
       String choice;
       do {
@@ -40,10 +46,11 @@ public class SecretSanta {
          System.out.println("Create (P)airings for the participants.");
          System.out.println("View (A)ll the pairings of participants.");
          System.out.println("View the recipient for (O)ne participant.");
-         System.out.println("(S)croll through each recipient for ALL participants one by one.");
+         System.out.println("(S)croll through each recipient for ALL " + 
+               "participants one by one.");
          System.out.println("(Q)uit.");
+         
          choice = console.nextLine();
-         System.out.println();
          if (choice.equalsIgnoreCase("c")) {
             participants = createParticipants(console);
          } else if (choice.equalsIgnoreCase("l")) {
@@ -57,13 +64,19 @@ public class SecretSanta {
          } else if (choice.equalsIgnoreCase("s")) {
             scrollAll(console, participants);
          }
+         
+         System.out.println();
       } while (!choice.equalsIgnoreCase("q"));
    }
    
+   // Prints modified text to highlight it
+   // Parameters:
+   //    String text = the desired text
    public static void printHighlighted(String text) {
       System.out.println("*** " + text + " ***");
    }
    
+   // Prints the introduction of the program
    public static void intro() {
       printHighlighted("SECRET SANTA");
       printHighlighted("This program allows you to add the participants of the");
@@ -71,6 +84,12 @@ public class SecretSanta {
       System.out.println();
    }
    
+   // Prompts the user for the number of participants 
+   // and the names of each participant.
+   // Parameters:
+   //    Scanner console = console for user input
+   // Returns:
+   //    list of Participant objects (Participant[])
    public static Participant[] createParticipants(Scanner console) {
       printHighlighted("Enter the number of participants desired");
       int num = promptInt(console);
@@ -78,30 +97,43 @@ public class SecretSanta {
          printHighlighted("Please enter a valid number.");
          num = promptInt(console);
       }
+      
       // to get rid of the \n created from nextInt() to fix Scanner's nextLine()
       console.nextLine(); 
+      
       Participant[] participants = new Participant[num];
       for (int i = 0; i < participants.length; i++) {
          System.out.println();
+         
          printHighlighted("Enter the name for participant #" + (i + 1));
          participants[i] = new Participant(console.nextLine());
       }
+      
       System.out.println();
       return participants;
    }
    
+   // Lists the names of each participant.
+   // Parameters:
+   //    Participant[] participants = the desired list of Participant objects
    public static void listParticipants(Participant[] participants) {
       for (int i = 0; i < participants.length; i++) {
          System.out.println((i + 1) + ": " + participants[i].getName());
       }
+      
       System.out.println();
    }
    
+   // Assigns the recipients of each participant.
+   // Parameters:
+   //    Participant[] participants = the desired list of Participant objects
+   //    Random rand = the random object
    public static void assignRecipients(Participant[] participants, 
          Random rand) {
       boolean allHaveMatches = false;
       while (!allHaveMatches){
          resetMatches(participants);
+         
          for (int i = 0; i < participants.length; i++) {
             printHighlighted("SEARCH FOR: \"" + 
                   participants[i].getName().toUpperCase() + 
@@ -119,7 +151,8 @@ public class SecretSanta {
                   participants[num].setSender(participants[i]);
                   System.out.println("Random participant found.");
                } else if (num == i) { // does not count towards retryCount
-                  System.out.println("Random participant was themselves...RETRYING.");
+                  System.out.println("Random participant was themselves..." + 
+                        "RETRYING.");
                } else if (participants[num].hasSender()) {
                   System.out.println("Random participant was \"" + 
                         participants[num].getName() + 
@@ -136,6 +169,7 @@ public class SecretSanta {
                   printHighlighted("OVER RETRY THRESHOLD...WILL RESET AFTER LOOP");
                }
             }
+            
             System.out.println();
          }
          
@@ -147,10 +181,16 @@ public class SecretSanta {
             System.out.println();
             printHighlighted("RESTARTING LOOP");
          }
+         
          System.out.println();
       }
    }
    
+   // Checks if all participants have matches/pairings
+   // Parameters:
+   //    Participant[] participants = the desired list of Participant objects
+   // Returns:
+   //    whether or not all participants have matches/pairings (boolean)
    public static boolean allHaveMatches(Participant[] participants) {
       for (Participant person : participants) {
          if (!person.hasRecipient() || !person.hasSender()) {
@@ -160,6 +200,9 @@ public class SecretSanta {
       return true;
    }
    
+   // Reset all matches/pairings of the participants
+   // Parameters:
+   //    Participant[] participants = the desired list of Participant objects
    public static void resetMatches(Participant[] participants) {
       for (Participant person : participants) {
          person.resetRecipient();
@@ -167,8 +210,12 @@ public class SecretSanta {
       }
    }
    
+   // Prints all matches/pairings of the participants
+   // Parameters:
+   //    Participant[] participants = the desired list of Participant objects
    public static void printAll(Participant[] participants) {
       printHighlighted("MATCHES OF ALL PARTICIPANTS");
+      
       if (allHaveMatches(participants)) {
          for (Participant person : participants) {
             System.out.println(person.getName() + 
@@ -178,13 +225,21 @@ public class SecretSanta {
                   person.getRecipientName());
          }
       } else {
-         printHighlighted("There are no pairings created for the participants yet.");
+         printHighlighted("There are no pairings created for the " + 
+               "participants yet.");
       }
+      
       System.out.println();
    }
    
+   // Lists the names of all participants, prompts the user for a participant,
+   // and prints the desired match/pairing of a specific participant
+   // Parameters:
+   //    Scanner console = console for user input
+   //    Participant[] participants = the desired list of Participant objects
    public static void printSpecific(Scanner console, Participant[] participants) {
       printHighlighted("VIEW SPECIFIC RECIPIENT OF A PARTICIPANT");
+      
       if (allHaveMatches(participants)) {
          printHighlighted("Select the number of a specific participant");
          listParticipants(participants);
@@ -194,13 +249,16 @@ public class SecretSanta {
             printHighlighted("Please enter a valid number.");
             participantChoice = promptInt(console);
          }
+         
          System.out.println();
          
          int participantIndex = participantChoice - 1;
          System.out.println(participants[participantIndex].getName() + 
                " will send a gift to: " + 
                participants[participantIndex].getRecipientName());
+               
          System.out.println();
+         
          // to get rid of the \n created from nextInt() to fix Scanner's nextLine()
          console.nextLine(); 
          
@@ -208,42 +266,58 @@ public class SecretSanta {
          console.nextLine();
          clearScreen(50);
       } else {
-         printHighlighted("There are no pairings created for the participants yet.");
+         printHighlighted("There are no pairings created for the " + 
+               "participants yet.");
          System.out.println();
       }
    }
    
+   // Lists the desired match/pairing of each participant one by one
+   // Parameters:
+   //    Scanner console = console for user input
+   //    Participant[] participants = the desired list of Participant objects
    public static void scrollAll(Scanner console, Participant[] participants) {
       printHighlighted("VIEW EACH RECIPIENT OF ALL PARTICIPANTS ONE BY ONE");
+      
       if (allHaveMatches(participants)) {
          for (int i = 0; i < participants.length; i++) {
             System.out.println();
             System.out.println(participants[i].getName() + 
                   " will send a gift to: " + 
                   participants[i].getRecipientName());
+                  
             System.out.println();
-            printHighlighted("Press enter to clear the output or enter Q to go back to the main menu");
+            
+            printHighlighted("Press enter to clear the output or enter Q to " + 
+                  "go back to the main menu");
             String scrollChoice = console.nextLine();
             if (scrollChoice.equalsIgnoreCase("Q")) {
                System.out.println();
                break;
             }
+            
             clearScreen(50);
+            
             if (i + 1 < participants.length) {
-               printHighlighted("Press enter to see the recipient for the next participant: " + 
-                     participants[i].getRecipientName() + " (" + 
+               printHighlighted("Press enter to see the recipient for the next " + 
+                     "participant: " + participants[i].getRecipientName() + " (" + 
                      (participants.length - i - 1) + " left)");
             } else {
-               printHighlighted("Press enter or enter Q to go back to the main menu");
+               printHighlighted(
+                     "Press enter or enter Q to go back to the main menu");
                System.out.println();
             }
          } 
       } else {
-         printHighlighted("There are no pairings created for the participants yet.");
+         printHighlighted(
+               "There are no pairings created for the participants yet.");
          System.out.println();
       }
    }
-
+   
+   // Floods the output by printing blank lines
+   // Parameters:
+   //    int num = the desired number of lines to flood the output
    public static void clearScreen(int num) {
       for (int i = 0; i < num; i++) {
          System.out.println(); 
@@ -251,6 +325,9 @@ public class SecretSanta {
       }  
    }
    
+   // Prompts the user for an integer
+   // Parameters:
+   //    Scanner console = console for user input
    public static int promptInt(Scanner console) {
       int num = 0;
       try {
